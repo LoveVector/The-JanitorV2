e8468822aab0b9eb35c07d9d779f1215b5f7fd3f
belligerentAlien <superprunejuice@gmail.com>
2020-05-16 18:33:47 +0400

    Pushed my stuff Integrate away ( AI Shooting enemy + Hat Asset + Machine Gun Asset ) Check my KyleLaihee/Prefab/Model/Model

---
+++ Assets/JanitorGame/Kyle Laihee Folder/Scripts/EnemyRangedAi.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRangedAi : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
+
+    private float shootRate;
+    public float startShootRate;
+
+    public GameObject bulletPrefab;
+    public Transform player;
+    public Transform gun;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        shootRate = startShootRate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyMovement();
+    }
+
+    void EnemyMovement()
+    {
+        transform.LookAt(2 * transform.position - player.position); // look towards player
+
+        if (Vector3.Distance(transform.position, player.position) > stoppingDistance) // Stop to shoot
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+        else if (Vector3.Distance(transform.position, player.position) < stoppingDistance && Vector3.Distance
+            (transform.position, player.position) > retreatDistance) // Close enough, SHoot
+        {
+            transform.position = this.transform.position;
+            EnemyShooting();
+        }
+        else if (Vector3.Distance(transform.position, player.position) < retreatDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+    }
+
+    void EnemyShooting()
+    {
+        if(shootRate <= 0)
+        {
+            Instantiate(bulletPrefab, gun.position, Quaternion.identity);
+            shootRate = startShootRate;
+        }
+        else
+        {
+            shootRate -= Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/JanitorGame/Kyle Laihee Folder/Scripts/ShootingScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour
+{
+    public float speed;
+    public float bulletLifeSpan;
+    public float updatePlayerPos;
+    Vector3 generalPlayerPosition;
+
+    public const float generalPositionRadius = 2f;
+
+    private Transform player;
+
+    private Vector3 target;
+
+    // ------------- Seek ----------------
+    private static Vector3 velocity;
+    private Vector3 desiredVelocity;
+    private Vector3 steering;
+
+    public float maxSpeed;
+    public float maxForce;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        //generalPlayerPosition = Random.insideUnitSphere * generalPositionRadius + player.position;
+        target = new Vector3(player.position.x, player.position.y, player.position.z);
+    }
+
+    void Update()
+    {
+        //transform.position = Vector3.MoveTowards(transform.position, generalPlayerPosition, speed * Time.deltaTime);   
+        SeekVelocity();
+
+        if (bulletLifeSpan <= 0)
+        {
+            DestroyBullet();
+        }
+        else
+        {
+            bulletLifeSpan -= Time.deltaTime;
+        }
+
+        if (transform.position.x == target.x && transform.position.y == target.y)
+        {
+            DestroyBullet();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DestroyBullet();
+        }
+    }
+
+    void SeekVelocity()
+    {
+        desiredVelocity = target - transform.position;
+        steering = desiredVelocity - velocity;
+        steering = Vector3.ClampMagnitude(steering, maxForce);
+
+        velocity = Vector3.ClampMagnitude(velocity + steering, maxSpeed);
+        transform.position += velocity * speed * Time.deltaTime;
+        transform.forward = velocity;
+    }
+
+    void DestroyBullet()
+    {
+        Destroy(gameObject);
+    }
+}
